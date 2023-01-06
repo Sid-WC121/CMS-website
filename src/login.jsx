8d5d09e7cc2cb3b9e884d6572b52824c640772bd
Sid-WC121 <112648016+Sid-WC121@users.noreply.github.com>
2023-01-06 07:32:04 +0530

    server update

--- src/login.jsx
+++ src/login.jsx
@@ -48,28 +48,18 @@ sessionStorage.clear();
     }
     return (
         <div className="row">
-            <div className="offset-lg-3 col-lg-6" style={{ marginTop: '100px' }}>
-                <form onSubmit={ProceedLogin} className="container">
-                    <div className="card">
-                        <div className="card-header">
-                            <h2>User Login</h2>
-                        </div>
-                        <div className="card-body">
-                            <div className="form-group">
-                                <label>User Name <span className="errmsg">*</span></label>
-                                <input value={username} onChange={e => usernameupdate(e.target.value)} className="form-control"></input>
-                            </div>
-                            <div className="form-group">
-                                <label>Password <span className="errmsg">*</span></label>
-                                <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control"></input>
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <button type="submit" className="btn btn-primary">Login</button>
-                        </div>
+            <form onSubmit={ProceedLogin} className="container">
+                <div className="card">
+                    <h2>User Login</h2>
+                    <div className="card-body" style={{alignItems: "center"}}>
+                        <input value={username} onChange={e => usernameupdate(e.target.value)} className="form-control" placeholder="Username"></input>
+                        <br/>
+                        <input type="password" value={password} onChange={e => passwordupdate(e.target.value)} className="form-control" placeholder="Password"></input>
                     </div>
-                </form>
-            </div>
+                    <br/>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                </div>
+            </form>
         </div>
     );
 }
