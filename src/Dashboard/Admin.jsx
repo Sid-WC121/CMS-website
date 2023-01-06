6b0480cf23b45c3cf9f2f3f1f212e32cdb4295b1
Sid-WC121 <112648016+Sid-WC121@users.noreply.github.com>
2023-01-07 00:04:53 +0530

    Dashboard initial files

---
+++ src/Dashboard/Admin.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Admin = () => {
+    const usenavigate = useNavigate();
+    useEffect(() => {
+        let username=sessionStorage.getItem('username');
+        if(username==='' || username ===null){
+            usenavigate('/login');
+        }
+    }, []);
+
+    return (
+        <div>
+            <div className="header">
+                <Link to={'/'}>Home</Link>
+                <Link style={{float:'right'}} to={'/login'}>Logout</Link>
+            </div>
+            <h1 className="text-center">AdminDashboard</h1>
+        </div>
+    );
+}
+
+export default Admin;
