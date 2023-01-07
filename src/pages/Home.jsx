8e0156554657cc73e4c4c25286b6ad099fde9b45
Sid-WC121 <112648016+Sid-WC121@users.noreply.github.com>
2023-01-07 18:04:03 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,20 +16,24 @@ const Home = () => {
     }, [])
     const usenavigate = useNavigate();
     useEffect(() => {
-        let username=sessionStorage.getItem('username');
-        if(username==='' || username ===null){
+        let username = sessionStorage.getItem('username');
+        if (username === '' || username === null) {
             usenavigate('/login');
+        }
+        if (username === 'admins') {
+            setShow(true)
         }
     }, []);
 
+    // Buttons are kept temperorly for testing logic
     return (
         <div>
             <div>
                 {
-                    show?<Link to={'/admin'}>Admin</Link>:null
+                    show ? <Link to={'/admin'}>Admin</Link> : null
                 }
-                <button onClick={()=>setShow(true)}>Show</button>
-                <button onClick={()=>setShow(false)}>Hide</button>
+                <button onClick={() => setShow(true)}>Show</button>
+                <button onClick={() => setShow(false)}>Hide</button>
                 <Link to={'/mentor'}>Mentor</Link>
             </div>
             <div className="header">
@@ -38,17 +42,17 @@ const Home = () => {
                 <Link to={'/members'}>Members</Link>
                 <Link to={'/research'}>Research</Link>
                 <Link to={'/projects'}>Projects</Link>
-                <Link style={{float:'right'}} to={'/login'}>Logout</Link>
+                <Link style={{ float: 'right' }} to={'/login'}>Logout</Link>
             </div>
             <h1 className="text-center">Bi0s Hardware</h1>
             <div>
-            {users.length > 0 && (
-                <ul>
-                {users.map(user => (
-                    <li key={user.id}>{user.name}</li>
-                ))}
-                </ul>
-            )}
+                {users.length > 0 && (
+                    <ul>
+                        {users.map(user => (
+                            <li key={user.id}>{user.name}</li>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     );
